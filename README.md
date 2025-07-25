a351520a32f6850dc547605b956d1e1f8cdd8af8
Ludovic Estival <ludovic.estival1@gmail.com>
2025-07-25 11:28:17 +0200

    Add project version 1

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# python-image-cli
-Python CLI app to manipulate images
+# Image CLI made with Python
+
+## Features
+
+Features:
+- convert WEBP images into another format
+- create a thumbnail of a picture
+
+## Installation
+
+It's recommended to use a Python virtual environment: `python -m venv venv`
+
+Activation on Linux: `source venv/bin/activate`
+
+Activation on Linux: `.\venv\Scripts\activate`
+
+Install dependencies: `pip install -r requirements.txt`
+
+
+## Usage
+
+Convert WEBP images: `python images.py convert-webp DIRECTORY FORMAT`
+
+Example: `python images.py convert-webp images-to-convert png`
+
+You can use option `--delete` to delete original pictures when converted.
+
+---
+
+Create a thumbnail: `python images.py thumbnail FILE WIDTH HEIGHT OUTPUT`
+
+Example: `python images.py thumbnail picture.jpg 450 600 picture-thumbnail.jpg`
---
+++ images.py
@@ -0,0 +1,52 @@
+"""Images manipulations."""
+
+# requirements: typer, pillow
+
+import os
+from typing_extensions import Annotated
+from PIL import Image, ImageOps
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def convert_webp(
+    directory: Annotated[str, typer.Argument(help='Directory of pictures to convert')],
+    img_format: Annotated[str, typer.Argument(help='Format to convert pictures into')] = 'jpeg',
+    delete: Annotated[bool, typer.Argument(help='Delete original pictures when converted')] = True):
+    """Convert a WEBP image into another format."""
+
+    paths = []
+    for entry in os.scandir(directory):
+        if entry.path.endswith('webp'):
+            paths.append(entry.path)
+
+    count = 1
+    for path in paths:
+        new_name = path.split('.webp')[0]
+        img = Image.open(path).convert('RGB')
+        img.save(f'{new_name}.{img_format}', img_format)
+        print(f'{count}/{len(paths)} done')
+        count += 1
+
+        if delete:
+            os.remove(path)
+
+
+@app.command()
+def thumbnail(
+    image: Annotated[str, typer.Argument(help='Image to create a thumbnail of')],
+    width: Annotated[str, typer.Argument(help='Width of the thumbnail')],
+    height: Annotated[str, typer.Argument(help='Height of the thumbnail')],
+    output: Annotated[str, typer.Argument(help='Name of the thumbnail')]
+):
+    """Create a thumbnail of a picture."""
+
+    image = Image.open(image)
+    fixed_image = ImageOps.exif_transpose(image)
+    fixed_image.thumbnail((int(width), int(height)))
+    fixed_image.save(output)
+
+
+if __name__ == '__main__':
+    app()
